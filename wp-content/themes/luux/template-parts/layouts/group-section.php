2f608a59de3793ec5d281ad57e183e76c47e7eaf
aixa-axl <aixa@axl-dev.com>
2026-07-07 17:35:08 +0200

    push

--- wp-content/themes/luux/template-parts/layouts/group-section.php
+++ wp-content/themes/luux/template-parts/layouts/group-section.php
@@ -9,7 +9,7 @@ $logos   = get_sub_field('logos');
 ?>
 
 <section class="bg-brand-dark p-10 lg:section-pad">
-    <div class="container-site flex flex-col gap-8 lg:flex-row lg:items-center lg:gap-20">
+    <div class="container-site flex flex-col gap-8 xl:flex-row xl:items-center xl:gap-20">
         <?php if ($heading || $text) : ?>
             <div class="flex flex-1 flex-col gap-4 text-brand-white">
                 <?php if ($heading) : ?>
@@ -22,11 +22,11 @@ $logos   = get_sub_field('logos');
         <?php endif; ?>
 
         <?php if ($logos) : ?>
-            <div class="grid grid-cols-2 gap-4 lg:flex lg:flex-wrap lg:gap-8">
+            <div class="grid grid-cols-2 gap-4 xl:flex xl:flex-wrap xl:gap-8">
                 <?php foreach ($logos as $logo) :
                     if (empty($logo['image'])) continue;
                     ?>
-                    <div class="flex h-14 w-full items-center justify-center lg:w-40">
+                    <div class="flex h-14 w-full items-center justify-center xl:w-40">
                         <?php echo wp_get_attachment_image($logo['image'], 'medium', false, [
                             'class'   => 'max-h-full max-w-full object-contain',
                             'loading' => 'lazy',
