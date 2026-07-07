d2bc445ec76252588e2d78ea141b0792f31c23a1
aixa-axl <aixa@axl-dev.com>
2026-07-07 13:56:36 +0200

    push

--- wp-content/themes/luux/template-parts/layouts/video-tours.php
+++ wp-content/themes/luux/template-parts/layouts/video-tours.php
@@ -3,23 +3,49 @@
  * Layout: video-tours
  */
 
-$heading     = get_sub_field('heading');
-$text        = get_sub_field('text');
-$image_left  = get_sub_field('image_left');
-$image_right = get_sub_field('image_right');
-$section_id  = get_sub_field('section_id');
+$heading          = get_sub_field('heading');
+$text             = get_sub_field('text');
+$media_type_left  = get_sub_field('media_type_left') ?: 'image';
+$image_left       = get_sub_field('image_left');
+$video_left       = get_sub_field('video_left');
+$media_type_right = get_sub_field('media_type_right') ?: 'image';
+$image_right      = get_sub_field('image_right');
+$video_right      = get_sub_field('video_right');
+$section_id       = get_sub_field('section_id');
+
+/**
+ * Render a single media slot as either a looping video or an image.
+ */
+$render_media = static function ($type, $image_id, $video_id) {
+    $is_video = ($type === 'video' && $video_id);
+    if (! $is_video && ! $image_id) {
+        return;
+    }
+    ?>
+    <div class="video-tours__media">
+        <?php if ($is_video) :
+            $video_url  = wp_get_attachment_url($video_id);
+            $video_mime = get_post_mime_type($video_id);
+            ?>
+            <video class="h-full w-full object-cover" autoplay muted loop playsinline>
+                <?php if ($video_url) : ?>
+                    <source src="<?php echo esc_url($video_url); ?>"<?php echo $video_mime ? ' type="' . esc_attr($video_mime) . '"' : ''; ?>>
+                <?php endif; ?>
+            </video>
+        <?php else : ?>
+            <?php echo wp_get_attachment_image($image_id, 'large', false, [
+                'class'   => 'h-full w-full object-cover',
+                'loading' => 'lazy',
+            ]); ?>
+        <?php endif; ?>
+    </div>
+    <?php
+};
 ?>
 
 <section<?php echo $section_id ? ' id="' . esc_attr($section_id) . '"' : ''; ?> class="video-tours section-pad">
     <div class="container-site video-tours__grid">
-        <?php if ($image_left) : ?>
-            <div class="video-tours__media">
-                <?php echo wp_get_attachment_image($image_left, 'large', false, [
-                    'class'   => 'h-full w-full object-cover',
-                    'loading' => 'lazy',
-                ]); ?>
-            </div>
-        <?php endif; ?>
+        <?php $render_media($media_type_left, $image_left, $video_left); ?>
 
         <?php if ($heading || $text) : ?>
             <div class="video-tours__copy">
@@ -32,13 +58,6 @@ $section_id  = get_sub_field('section_id');
             </div>
         <?php endif; ?>
 
-        <?php if ($image_right) : ?>
-            <div class="video-tours__media">
-                <?php echo wp_get_attachment_image($image_right, 'large', false, [
-                    'class'   => 'h-full w-full object-cover',
-                    'loading' => 'lazy',
-                ]); ?>
-            </div>
-        <?php endif; ?>
+        <?php $render_media($media_type_right, $image_right, $video_right); ?>
     </div>
 </section>
