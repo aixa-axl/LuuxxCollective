caec979d2e3a87dc7889950e2d45c42fa0290518
aixa-axl <aixa@axl-dev.com>
2026-07-08 11:10:15 +0200

    push

--- wp-content/themes/luux/template-parts/layouts/cta-strip.php
+++ wp-content/themes/luux/template-parts/layouts/cta-strip.php
@@ -9,13 +9,13 @@ $secondary_link = get_sub_field('secondary_link');
 ?>
 
 <section class="bg-brand-dark p-10 lg:section-pad">
-    <div class="container-site flex flex-col items-start gap-8 text-left lg:flex-row lg:items-center lg:justify-between lg:gap-6 lg:text-left">
+    <div class="container-site flex flex-col items-center gap-8 text-center lg:flex-row lg:items-center lg:justify-between lg:gap-6 lg:text-left">
         <?php if ($text) : ?>
             <p class="font-body text-body-lg text-brand-white lg:max-w-none"><?php echo esc_html($text); ?></p>
         <?php endif; ?>
 
         <?php if ($primary_link || $secondary_link) : ?>
-            <div class="flex w-full flex-col gap-4 lg:w-auto lg:flex-row lg:gap-10">
+            <div class="flex w-full flex-col items-center gap-4 lg:w-auto lg:flex-row lg:gap-10">
                 <?php if (! empty($primary_link['url'])) : ?>
                     <a class="link-underline-block link-underline-block--ruled text-brand-white"
                        href="<?php echo esc_url($primary_link['url']); ?>"
